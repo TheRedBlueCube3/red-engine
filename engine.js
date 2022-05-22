c507e30e700e755ec06bb5f4c39b96e22ef7a738
TheRedBlueCube3 <98588842+TheRedBlueCube3@users.noreply.github.com>
2022-05-22 22:29:34 +0300

    Added JSDoc support

--- engine.js
+++ engine.js
@@ -1,16 +1,32 @@
 class Component {
+	/**
+	 * The constructor.
+	 * @param {string} selector The selector
+	 */
 	constructor(selector) {
 		this.$el = document.querySelector(selector);
 	}
+	/**
+	 * Hides the associated item.
+	 */
 	hide() {
 		this.$el.style.display = 'none';
 	}
+	/**
+	 * _(opposite of {@link hide()})_
+	 * Shows the associated item.
+	 */
 	show() {
 		this.$el.style.display = 'block';
 	}
 }
 
 class Box extends Component {
+	/**
+	 *
+	 * @param {object} options The box's options.
+	 * @param {string} options.selector The box's selector.
+	 */
 	constructor(options) {
 		super(options.selector);
 		this.$el.style.width = this.$el.style.height = options.size + 'px';
